7e6a31134872a31116354d0e88f8a0a8b55c90f0
Saleh0987 <ms3617224@gmail.com>
2024-10-07 17:32:44 +0300

    edit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 5080;
+
+// Import products
+const products = require("./data/products.json");
 
 // Enable CORS for all origins
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -14,22 +16,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/products", (req, res) => {
-  const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: 10,
-      description: "test test test test",
-      image: "https://shorturl.at/gbc3p",
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: 20,
-      description: "test test test test",
-      image: "https://shorturl.at/C8CCO",
-    },
-  ];
   res.json(products);
 });
 
