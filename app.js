adfef0618f894361a09e927c109eb3d9913663b2
Saleh0987 <ms3617224@gmail.com>
2024-10-08 16:02:59 +0300

    edit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// index.js
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = 5080;
 
@@ -8,20 +8,22 @@ const port = 5080;
 const products = require("./data/products.json");
 const categories = require("./data/categories.json");
 
-
 // Enable CORS for all origins
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello, API!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/api/products", (req, res) => {
+app.get("/products", (req, res) => {
   res.json(products);
 });
 
-app.get("/api/categories", (req, res) => {
+app.get("/categories", (req, res) => {
   res.json(categories);
 });
 
